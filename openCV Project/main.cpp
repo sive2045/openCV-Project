74e71346d38c88481b581d423cb0313e7b1a9c71
Chung Neung Lee <lc9902130509@gmail.com>
2020-08-05 01:36:09 +0900

    day1

---
+++ openCV Project/main.cpp
@@ -0,0 +1,32 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat img_color;
+
+	img_color = imread("download.jpg", IMREAD_COLOR);
+	if (img_color.empty())
+	{
+		cout << "파일을 열 수 없습니다!" << endl;
+		return -1;
+	}
+
+	Mat img_gray;
+
+	cvtColor(img_color, img_gray, COLOR_BGR2GRAY);
+	imwrite("gray image.jpg", img_gray);
+
+	namedWindow("BGR", WINDOW_AUTOSIZE);
+	namedWindow("Grayscale", WINDOW_AUTOSIZE);
+
+	imshow("BGR", img_color);
+	imshow("Grayscale", img_gray);
+
+	while (waitKey(0) != 27);
+
+	return 0;
+}
