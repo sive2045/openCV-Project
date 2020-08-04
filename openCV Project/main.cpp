d17e64993c02bb1735b2fe32d7d891a69264c4dc
Chung Neung Lee <lc9902130509@gmail.com>
2020-08-05 03:15:31 +0900

    수동으로 GRAY변환하기

--- openCV Project/main.cpp
+++ openCV Project/main.cpp
@@ -15,16 +15,38 @@ int main()
 		return -1;
 	}
 
-	Mat img_gray;
+	Mat img_gray_3channel, img_gray_1channel;
+	img_gray_3channel.create(img_color.rows, img_color.cols, CV_8UC3);
+	img_gray_1channel.create(img_color.rows, img_color.cols, CV_8UC1);
 
-	cvtColor(img_color, img_gray, COLOR_BGR2GRAY);
-	imwrite("gray image.jpg", img_gray);
+	for (int y = 0; y < img_color.rows; y++)
+	{
+		for (int x = 0; x < img_color.cols; x++)
+		{
+			//2
+			uchar b = img_color.at<Vec3b>(y, x)[0];
+			uchar g = img_color.at<Vec3b>(y, x)[1];
+			uchar r = img_color.at<Vec3b>(y, x)[2];
 
-	namedWindow("BGR", WINDOW_AUTOSIZE);
-	namedWindow("Grayscale", WINDOW_AUTOSIZE);
+			//3
+			uchar gray = (b + g + r) / 3.0;
 
-	imshow("BGR", img_color);
-	imshow("Grayscale", img_gray);
+
+			//4
+			img_gray_1channel.at<uchar>(y, x) = gray;
+
+
+
+			//5
+			img_gray_3channel.at<Vec3b>(y, x)[0] = gray;
+			img_gray_3channel.at<Vec3b>(y, x)[1] = gray;
+			img_gray_3channel.at<Vec3b>(y, x)[2] = gray;
+		}
+	}
+
+
+	imshow("Grayscale 3 channel", img_gray_3channel);
+	imshow("Grayscale 1 channel", img_gray_1channel);
 
 	while (waitKey(0) != 27);
 
