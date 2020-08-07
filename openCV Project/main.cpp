c1bf3dd845a9f9f74d6b68dd61deb1a31ccda4b9
Chung Neung Lee <lc9902130509@gmail.com>
2020-08-08 04:07:50 +0900

    ROI Example

--- openCV Project/main.cpp
+++ openCV Project/main.cpp
@@ -7,28 +7,31 @@ using namespace cv;
 
 int main()
 {
-	Mat img_1, img_2;
-	double alpha = 0.0;
-	double beta = 1.0;
+	Mat img;
+	img = imread("love.jpg", IMREAD_COLOR);
+
+	int height = img.rows;
+	int width = img.cols;
+
+	int center_x = (int)width*0.5;
+	int center_y = (int)height*0.5;
+
+	//깊은 복사를 통해 원본 유지한채 canny처리
+	Mat img_roi;
+	img_roi = img(Rect(center_x - 100, center_y - 100, 200, 200)).clone();
+	Mat img_gray;
+	cvtColor(img_roi, img_gray, COLOR_BGR2GRAY);
+	//canny처리후 BGR체널로 전환
+	Mat img_edge;
+	Canny(img_gray, img_edge, 100, 300);
+	cvtColor(img_edge, img_edge, COLOR_GRAY2BGR);
+	//원본에 합셩
+	img_edge.copyTo(img(Rect(center_x-100,center_y-100,200,200)));
+	//결과 보여주기
 	
-	while (alpha <= 1.0)
-	{
-		//addWeight처리하기위해서는 매번 불러와야함. 
-		//두 이미지 크기는 같아야 함.
-		img_1 = imread("love.jpg", IMREAD_COLOR);
-		img_2 = imread("back.jpg", IMREAD_COLOR);
-
-		Mat dst;
-		addWeighted(img_1, alpha, img_2, beta, 0, dst);
-
-		cout << alpha << " " << beta << endl;
-
-		imshow("Result", dst);
-		waitKey(0);
-
-		alpha += 0.1;
-		beta -= 0.1;
-	}
+	imshow("ROI", img_roi);
+	imshow("Result", img);
+	waitKey(0);
 
 	return 0;
 }
