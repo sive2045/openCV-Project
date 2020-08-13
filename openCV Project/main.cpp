c8e10017e108b9b88341f9785cebb72dfda6544c
Chung Neung Lee <lc9902130509@gmail.com>
2020-08-14 04:01:39 +0900

    Morphology Closing Example by recit

--- openCV Project/main.cpp
+++ openCV Project/main.cpp
@@ -4,37 +4,21 @@
 using namespace std;
 using namespace cv;
 
-static void on_trackbar(int, void*)
-{
 
-}
 
 int main()
 {
-	namedWindow("Canny Edge");
-	createTrackbar("low threshold", "Canny Edge", 0, 1000, on_trackbar);
-	createTrackbar("high threshold", "Canny Edge", 0, 1000, on_trackbar);
-	setTrackbarPos("low threshold", "Canny Edge", 50);
-	setTrackbarPos("high threshold", "Canny Edge", 150);
-	Mat img;
-	img = imread("1.png", IMREAD_COLOR);
-	Mat img_gray;
-	cvtColor(img, img_gray, COLOR_BGR2GRAY);
-	imshow("Original", img_gray);
-	Mat img_edge;
-	blur(img_gray, img_gray, Size(3, 3));
-
-	while (1)
-	{
-		int low = getTrackbarPos("low threshold", "Canny Edge");
-		int high = getTrackbarPos("high threshold", "Canny Edge");
-
-		Canny(img_gray, img_edge, low, high, 3);
-		imshow("Canny Edge", img_edge);
-
-		if (waitKey(1) == 27)
-			break;
-	}
+	Mat img = imread("123.jpg", IMREAD_GRAYSCALE);
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(15, 15));
+	Mat img_result;
+	morphologyEx(img, img_result, MORPH_CLOSE, kernel);
+	imshow("Op", img_result);
+	threshold(img_result, img_result, 200, 255, THRESH_BINARY);
+	
+	imshow("Original", img);
+	imshow("Output", img_result);
+	
+	waitKey(0);
 
 	return 0;
 }
